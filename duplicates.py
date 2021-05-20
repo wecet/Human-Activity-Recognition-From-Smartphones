c62b745cb1bdc1a3d2985b8f6a402a0ec588ea55
Owen Agius <73174341+Fliinko@users.noreply.github.com>
2021-05-20 21:55:37 +0200

    Update duplicates.py

--- duplicates.py
+++ duplicates.py
@@ -10,7 +10,7 @@ print("Size of File with Duplicates: \n", len(file))
 
 ##REMOVED IS THE VARIABLE THAT STORES THE DATA SET WITHOUT DUPLICATES
 removed = file.drop_duplicates(keep='last')
-removed.to_csv("output.csv")
+removed.to_csv("output.csv", index = False)
 print("Size of File after Removal of Duplicates: \n", len(removed))
 #print(type(removed))
 #print("\nResult after Removal :\n", removed.head(n=5))
